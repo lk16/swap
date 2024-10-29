d86daee552f3c700aa5cf96e6ac53d0d75250657
Luuk Verweij <luuk_verweij@msn.com>
2024-10-29 19:07:01 +0100

    simplify undo/redo in Game even further

--- src/othello/game.rs
+++ src/othello/game.rs
@@ -2,8 +2,13 @@ use super::{board::Board, position::GameState};
 use crate::bot::{get_bot, Bot};
 
 pub struct Game {
-    history: Vec<Board>,
-    undone_moves: Vec<Board>, // For undo/redo
+    /// The boards in the game history
+    boards: Vec<Board>,
+
+    /// The index of the current board in the `boards` vector
+    offset: usize,
+
+    /// The bots for each player
     bots: [Option<Box<dyn Bot>>; 2],
 }
 
@@ -16,8 +21,8 @@ impl Default for Game {
 impl Game {
     pub fn new() -> Self {
         Self {
-            history: vec![Board::new()],
-            undone_moves: vec![],
+            boards: vec![Board::new()],
+            offset: 0,
             bots: [None, None],
         }
     }
@@ -44,36 +49,33 @@ impl Game {
 
     /// Returns true if the undo was successful
     pub fn undo(&mut self) -> bool {
-        // Find the index of the last human turn in the history
-        let last_human_turn = self
-            .history
-            .iter()
-            .rev()
-            .skip(1) // Skip current position
-            .position(|board| self.has_human_turn(board));
+        let mut moves_to_undo: Option<usize> = None;
+
+        for i in (0..self.offset).rev() {
+            if self.has_human_turn(&self.boards[i]) {
+                moves_to_undo = Some(self.offset - i);
+                break;
+            }
+        }
 
         // If no human turn is found, don't undo
-        let Some(last_human_index) = last_human_turn else {
+        let Some(moves_to_undo) = moves_to_undo else {
             return false;
         };
 
-        // Convert the reverse index to a forward index
-        let last_human_index = self.history.len() - 2 - last_human_index;
-
-        // Undo moves until we reach the last human turn
-        while self.history.len() > last_human_index + 1 {
-            let undone_move = self.history.pop().unwrap();
-            self.undone_moves.push(undone_move);
-        }
+        // Effectively undo the moves
+        self.offset -= moves_to_undo;
 
         true
     }
 
     /// Returns true if the redo was successful
     pub fn redo(&mut self) -> bool {
-        if let Some(redone_move) = self.undone_moves.pop() {
-            self.history.push(redone_move);
-            return true;
+        for i in self.offset + 1..self.boards.len() {
+            if self.has_human_turn(&self.boards[i]) {
+                self.offset = i;
+                return true;
+            }
         }
 
         false
@@ -81,23 +83,27 @@ impl Game {
 
     pub fn do_move(&mut self, move_index: usize) {
         let mut board = self.current_board().do_move_cloned(move_index);
-        self.history.push(board);
+
+        // Prevent redo now that a move has been made
+        self.boards.truncate(self.offset + 1);
+
+        self.boards.push(board);
+        self.offset += 1;
 
         if board.game_state() == GameState::Passed {
             board.pass();
-            self.history.push(board);
+            self.boards.push(board);
+            self.offset += 1;
         }
-
-        self.undone_moves.clear();
     }
 
     pub fn reset(&mut self, board: Board) {
-        self.history = vec![board];
-        self.undone_moves.clear();
+        self.boards = vec![board];
+        self.offset = 0;
     }
 
     pub fn current_board(&self) -> &Board {
-        self.history.last().unwrap()
+        &self.boards[self.offset]
     }
 }
 
@@ -110,8 +116,8 @@ mod tests {
     #[test]
     fn test_new() {
         let game = Game::new();
-        assert_eq!(game.history.len(), 1);
-        assert_eq!(game.undone_moves.len(), 0);
+        assert_eq!(game.boards.len(), 1);
+        assert_eq!(game.offset, 0);
         assert!(game.bots[0].is_none());
         assert!(game.bots[1].is_none());
     }
@@ -165,21 +171,21 @@ mod tests {
         // Make some moves
         game.do_move(19); // Human move
         game.do_move(26); // Bot move
-        assert_eq!(game.history.len(), 3);
+        assert_eq!(game.boards.len(), 3);
+        assert_eq!(game.offset, 2);
 
         // Test undo
         assert!(game.undo());
-        assert_eq!(game.history.len(), 1);
-        assert_eq!(game.undone_moves.len(), 2);
+        assert_eq!(game.boards.len(), 3);
+        assert_eq!(game.offset, 0);
 
         // Test redo
         assert!(game.redo());
-        // TODO this breaks:
-        // assert_eq!(game.history.len(), 3);
-        // assert_eq!(game.undone_moves.len(), 0);
+        assert_eq!(game.boards.len(), 3);
+        assert_eq!(game.offset, 2);
 
         // Test redo with no moves to redo
-        // assert!(!game.redo());
+        assert!(!game.redo());
     }
 
     #[test]
@@ -188,22 +194,22 @@ mod tests {
 
         // Test normal move
         game.do_move(19);
-        assert_eq!(game.history.len(), 2);
-        assert_eq!(game.undone_moves.len(), 0);
+        assert_eq!(game.boards.len(), 2);
+        assert_eq!(game.offset, 1);
 
         // Test move that forces a pass
         // Position: | - ● ○ ● -     |
         game.reset(Board::new_from_bitboards(0x4, 0xA, BLACK));
         game.do_move(0);
-        assert_eq!(game.history.len(), 3);
-        assert_eq!(game.undone_moves.len(), 0);
+        assert_eq!(game.boards.len(), 3);
+        assert_eq!(game.offset, 2);
 
         // Test move that ends the game
         // Position: | - ● ○         |
         game.reset(Board::new_from_bitboards(0x4, 0x2, BLACK));
         game.do_move(0);
-        assert_eq!(game.history.len(), 2);
-        assert_eq!(game.undone_moves.len(), 0);
+        assert_eq!(game.boards.len(), 2);
+        assert_eq!(game.offset, 1);
     }
 
     #[test]
@@ -213,13 +219,14 @@ mod tests {
         // Make some moves
         game.do_move(19);
         game.do_move(26);
-        assert_eq!(game.history.len(), 3);
+        assert_eq!(game.boards.len(), 3);
+        assert_eq!(game.offset, 2);
 
         // Reset with new board
         let new_board = Board::new();
         game.reset(new_board);
-        assert_eq!(game.history.len(), 1);
-        assert_eq!(game.undone_moves.len(), 0);
+        assert_eq!(game.boards.len(), 1);
+        assert_eq!(game.offset, 0);
     }
 
     #[test]
