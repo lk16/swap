a47ba5fa2f5f9ee2383217b1e28ee4f20e0b7072
Luuk Verweij <luuk_verweij@msn.com>
2024-11-08 14:23:34 +0100

    use edax flip bitscan for doing moves

--- src/othello/do_move/mod.rs
+++ src/othello/do_move/mod.rs
@@ -5,5 +5,5 @@ pub mod simple;
 use super::position::Position;
 
 pub fn do_move(position: &mut Position, index: usize) -> u64 {
-    simple::do_move_simple(position, index)
+    edax_bitscan::do_move_edax_bitscan(position, index)
 }
